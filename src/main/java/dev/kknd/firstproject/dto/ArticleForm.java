96d4408ea8da979b57a3d633861914827cdbca3e
andy <lycigold@gmail.com>
2023-12-25 16:46:06 +0900

    폼데이터 전송 설정 모델에 기본생성자를 만들어주어야한다.

---
+++ src/main/java/dev/kknd/firstproject/dto/ArticleForm.java
@@ -0,0 +1,39 @@
+package dev.kknd.firstproject.dto;
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm() {
+    }
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
