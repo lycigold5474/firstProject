eb15c9303926d9ddb40319fadb8344f270df9d47
andy <lycigold@gmail.com>
2023-12-26 16:54:42 +0900

    데이터조회하기 findById

--- src/main/java/dev/kknd/firstproject/controller/ArticleController.java
+++ src/main/java/dev/kknd/firstproject/controller/ArticleController.java
@@ -5,6 +5,7 @@ import dev.kknd.firstproject.entity.Article;
 import dev.kknd.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -61,5 +62,22 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info("saved = {}", saved);
         return form;
+    }
+
+    /**
+     * url요청받기
+     * @param id
+     * @return
+     */
+    @GetMapping("/articles/{id}") // 해당 URL요청을 처리 선언
+    public String show(@PathVariable(value="id") Long id, Model model) { // URL에서 id를 변수로 가져옴
+        log.info("id = " + id);
+
+        // 1: id로 데이터를 가져옴!
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2: 가져온 데이터를 모델에 등록!
+        model.addAttribute("article", articleEntity);
+        // 3: 보여줄 페이지를 설정!
+        return "articles/show";
     }
 }
