3b486f7e5c91820b22da822cd216d37e9465360f
andy <lycigold@gmail.com>
2024-01-06 23:49:12 +0900

    ArticleApiController 리팩토링 Service생성

---
+++ src/main/java/dev/kknd/firstproject/service/ArticleService.java
@@ -0,0 +1,63 @@
+package dev.kknd.firstproject.service;
+
+import dev.kknd.firstproject.dto.ArticleForm;
+import dev.kknd.firstproject.entity.Article;
+import dev.kknd.firstproject.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private ArticleRepository articleRepository;
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article show(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if (article.getId() != null) {
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        // 1: DTO -> 엔티티
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2: 타겟 조회
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3: 잘못된 요청 처리
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return null;
+        }
+        // 4: 업데이트
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return updated;
+    }
+
+    public Article delete(Long id) {
+        // 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        // 잘못된 요청 처리
+        if (target == null) {
+            return null;
+        }
+        // 대상 삭제
+        articleRepository.delete(target);
+        return target;
+    }
+}
