8f0ae31431d558251ed601d93cd9dc520ee5e2ab
andy <lycigold@gmail.com>
2024-01-06 22:18:07 +0900

    id 생성 전략

--- src/main/java/dev/kknd/firstproject/entity/Article.java
+++ src/main/java/dev/kknd/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package dev.kknd.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -14,7 +11,7 @@ import lombok.*;
 public class Article {
 
     @Id // 대표값을 지정
-    @GeneratedValue //자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동 생성 전략
     private Long id;
     @Column
     private String title;
