9e612373322e16c7b699902224a7b1ed778628c1
andy <lycigold@gmail.com>
2024-01-06 22:57:02 +0900

    http와 RestController

--- src/main/java/dev/kknd/firstproject/config/CorsConfig.java
+++ src/main/java/dev/kknd/firstproject/config/CorsConfig.java
@@ -7,15 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/articles/**")
-//                .allowedOrigins("http://localhost:8080")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("header1", "header2", "header3")
-                .exposedHeaders("header1", "header2")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**/**")
+////        registry.addMapping("/articles/**")
+////                .allowedOrigins("http://localhost:8080")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("header1", "header2", "header3")
+//                .exposedHeaders("header1", "header2")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
 }
