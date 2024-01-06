15178459f66717a54f262b6433d8a7e6462de035
andy <lycigold@gmail.com>
2024-01-06 22:35:55 +0900

    http request

--- src/main/java/dev/kknd/firstproject/config/CorsConfig.java
+++ src/main/java/dev/kknd/firstproject/config/CorsConfig.java
@@ -1,11 +1,6 @@
 package dev.kknd.firstproject.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
