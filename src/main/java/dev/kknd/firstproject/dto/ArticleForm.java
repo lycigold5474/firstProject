f12be2dee73da4baf32fc3ad9dacae6a8736a467
andy <lycigold@gmail.com>
2024-01-07 15:39:03 +0900

    테스트코드 추가작성

--- src/main/java/dev/kknd/firstproject/dto/ArticleForm.java
+++ src/main/java/dev/kknd/firstproject/dto/ArticleForm.java
@@ -1,10 +1,7 @@
 package dev.kknd.firstproject.dto;
 
 import dev.kknd.firstproject.entity.Article;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @RequiredArgsConstructor
 @Setter
@@ -14,6 +11,12 @@ public class ArticleForm {
     private Long id;
     private String title;
     private String content;
+
+    public ArticleForm(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
 
     public Article toEntity(){
         return new Article(id, title, content);
--- src/test/java/dev/kknd/firstproject/service/ArticleServiceTest.java
+++ src/test/java/dev/kknd/firstproject/service/ArticleServiceTest.java
@@ -1,6 +1,8 @@
 package dev.kknd.firstproject.service;
 
+import dev.kknd.firstproject.dto.ArticleForm;
 import dev.kknd.firstproject.entity.Article;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,11 +40,52 @@ class ArticleServiceTest {
     }
 
     @Test
-    void show() {
+    void show_성공____존재하는_id_입력() {
+        // 예상
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        // 실제
+        Article article = articleService.show(id);
+        // 비교
+        assertEquals(expected.toString(), article.toString());
     }
 
     @Test
-    void create() {
+    void show_실패____존재하지_않는_id_입력() {
+        // 예상
+        Long id = -1L;
+        Article expected = null;
+        // 실제
+        Article article = articleService.show(id);
+        // 비교
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void create_성공____title과_content만_있는_dto_입력() {
+        // 예상
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+        // 실제
+        Article article = articleService.create(dto);
+        // 비교
+        assertEquals(expected.toString(), article.toString());
+    }
+    @Test
+    @Transactional
+    void create_실패____id가_포함된_dto_입력() {
+        // 예상
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(4L, title, content);
+        Article expected = null;
+        // 실제
+        Article article = articleService.create(dto);
+        // 비교
+        assertEquals(expected, article);
     }
 
     @Test
