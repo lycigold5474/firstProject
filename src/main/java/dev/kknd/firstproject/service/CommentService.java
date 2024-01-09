9950805ca13665a93fa72ac4dc6aa6468ff7c72f
andy <lycigold@gmail.com>
2024-01-09 18:12:23 +0900

    반복을 스트림으로 변환 - stream(), map(), collect(), Collectors.toList()

--- src/main/java/dev/kknd/firstproject/service/CommentService.java
+++ src/main/java/dev/kknd/firstproject/service/CommentService.java
@@ -1,14 +1,13 @@
 package dev.kknd.firstproject.service;
 
 import dev.kknd.firstproject.dto.CommentDto;
-import dev.kknd.firstproject.entity.Comment;
 import dev.kknd.firstproject.repository.ArticleRepository;
 import dev.kknd.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -19,17 +18,11 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 조회: 댓글 목록
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-        // 변환: 엔티티 -> DTO
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);
-            dtos.add(dto);
-        }
         // 반환
-        return dtos;
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
     }
 
 
