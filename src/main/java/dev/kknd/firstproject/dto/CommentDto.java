5346e209f72f0b9bf9fc5eada09bb8c8683ee570
andy <lycigold@gmail.com>
2024-01-09 18:06:10 +0900

    댓글 목록 조회

---
+++ src/main/java/dev/kknd/firstproject/dto/CommentDto.java
@@ -0,0 +1,27 @@
+package dev.kknd.firstproject.dto;
+
+import dev.kknd.firstproject.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+    private Long id;
+    private Long articleId;
+    private String nicknmae;
+    private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+}
--- src/main/java/dev/kknd/firstproject/service/CommentService.java
+++ src/main/java/dev/kknd/firstproject/service/CommentService.java
@@ -1,9 +1,14 @@
 package dev.kknd.firstproject.service;
 
+import dev.kknd.firstproject.dto.CommentDto;
+import dev.kknd.firstproject.entity.Comment;
 import dev.kknd.firstproject.repository.ArticleRepository;
 import dev.kknd.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -12,4 +17,20 @@ public class CommentService {
 
     @Autowired
     private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 조회: 댓글 목록
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 변환: 엔티티 -> DTO
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }
+        // 반환
+        return dtos;
+    }
+
+
 }
