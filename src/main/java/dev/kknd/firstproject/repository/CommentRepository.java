9ad7f526823bfb9114e76a1da5f6fecbc9289740
andy <lycigold@gmail.com>
2024-01-09 17:37:39 +0900

    articleId를 찾지 못하는 에러 대처 @Param사용

--- src/main/java/dev/kknd/firstproject/repository/CommentRepository.java
+++ src/main/java/dev/kknd/firstproject/repository/CommentRepository.java
@@ -3,9 +3,12 @@ package dev.kknd.firstproject.repository;
 import dev.kknd.firstproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(
             value =
@@ -14,7 +17,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
                     "WHERE article_id = :articleId",
             nativeQuery = true
     )
-    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
 
     List<Comment> findByNickname(String nickname);
 }
